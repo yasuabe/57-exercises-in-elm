cdef4469a4477f9b0ded9e22c83545dad06ef8e3
yasuabe2613 <yasu@yasuabe.net>
2025-06-05 20:29:13 +0900

    ex02: solve

--- src/Main.elm
+++ src/Main.elm
@@ -6,30 +6,44 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Pages.Ex01 as Ex01
+import Pages.Ex02 as Ex02
 import Url exposing (Url)
 import Url.Parser as Parser exposing ((</>), int, s, top)
 
+
+
 -- MODEL
+
 
 type alias Model =
     { key : Nav.Key
     , currentExercise : Maybe Int
     , ex01Model : Maybe Ex01.Model
+    , ex02Model : Maybe Ex02.Model
+
     -- 必要に応じて他のExerciseのModelを追加
     }
 
+
+
 -- MSG
+
 
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url
     | Ex01Msg Ex01.Msg
+    | Ex02Msg Ex02.Msg
+
+
 
 -- ROUTE
+
 
 type Route
     = Home
     | Exercise Int
+
 
 routeParser : Parser.Parser (Route -> a) a
 routeParser =
@@ -38,6 +52,7 @@ routeParser =
         , Parser.map Exercise exerciseParser
         ]
 
+
 exerciseParser : Parser.Parser (Int -> a) a
 exerciseParser =
     Parser.custom "EXERCISE" <|
@@ -45,23 +60,46 @@ exerciseParser =
             if String.startsWith "ex" segment && String.length segment >= 3 then
                 String.dropLeft 2 segment
                     |> String.toInt
-                    |> Maybe.andThen (\n -> if n >= 1 && n <= 57 then Just n else Nothing)
+                    |> Maybe.andThen
+                        (\n ->
+                            if n >= 1 && n <= 57 then
+                                Just n
+
+                            else
+                                Nothing
+                        )
+
             else
                 Nothing
 
+
+
 -- INIT
+
 
 init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
     let
-        route = Parser.parse routeParser url |> Maybe.withDefault Home
-        (currentEx, initModel) = case route of
-            Exercise 1 -> (Just 1, Just Ex01.init)
-            _ -> (Nothing, Nothing)
+        route =
+            Parser.parse routeParser url |> Maybe.withDefault Home
+
+        ( currentEx, initModel1, initModel2 ) =
+            case route of
+                Exercise 1 ->
+                    ( Just 1, Just Ex01.init, Nothing )
+
+                Exercise 2 ->
+                    ( Just 2, Nothing, Just Ex02.init )
+
+                _ ->
+                    ( Nothing, Nothing, Nothing )
     in
-    ( { key = key, currentExercise = currentEx, ex01Model = initModel }, Cmd.none )
+    ( { key = key, currentExercise = currentEx, ex01Model = initModel1, ex02Model = initModel2 }, Cmd.none )
+
+
 
 -- UPDATE
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -70,41 +108,65 @@ update msg model =
             case urlRequest of
                 Browser.Internal url ->
                     ( model, Nav.pushUrl model.key (Url.toString url) )
+
                 Browser.External href ->
                     ( model, Nav.load href )
 
         UrlChanged url ->
             let
-                route = Parser.parse routeParser url |> Maybe.withDefault Home
+                route =
+                    Parser.parse routeParser url |> Maybe.withDefault Home
             in
             case route of
                 Exercise 1 ->
-                    ( { model | currentExercise = Just 1, ex01Model = Just Ex01.init }, Cmd.none )
+                    ( { model | currentExercise = Just 1, ex01Model = Just Ex01.init, ex02Model = Nothing }, Cmd.none )
+
+                Exercise 2 ->
+                    ( { model | currentExercise = Just 2, ex01Model = Nothing, ex02Model = Just Ex02.init }, Cmd.none )
+
                 _ ->
-                    ( { model | currentExercise = Nothing, ex01Model = Nothing }, Cmd.none )
+                    ( { model | currentExercise = Nothing, ex01Model = Nothing, ex02Model = Nothing }, Cmd.none )
 
         Ex01Msg subMsg ->
             case model.ex01Model of
                 Just ex01Model ->
                     let
-                        (newModel, cmd) = Ex01.update subMsg ex01Model
+                        ( newModel, cmd ) =
+                            Ex01.update subMsg ex01Model
                     in
                     ( { model | ex01Model = Just newModel }, Cmd.map Ex01Msg cmd )
+
                 Nothing ->
                     ( model, Cmd.none )
 
+        Ex02Msg subMsg ->
+            case model.ex02Model of
+                Just ex02Model ->
+                    let
+                        ( newModel, cmd ) =
+                            Ex02.update subMsg ex02Model
+                    in
+                    ( { model | ex02Model = Just newModel }, Cmd.map Ex02Msg cmd )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+
+
 -- VIEW
+
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Elm 57 Exercises"
-    , body = 
+    , body =
         [ div [ style "display" "flex", style "height" "100vh" ]
             [ leftPane
             , mainPane model
             ]
         ]
     }
+
 
 leftPane : Html Msg
 leftPane =
@@ -113,14 +175,17 @@ leftPane =
         , div [] (List.map exerciseLink (List.range 1 57))
         ]
 
+
 exerciseLink : Int -> Html Msg
 exerciseLink n =
     let
-        paddedNumber = String.padLeft 2 '0' (String.fromInt n)
+        paddedNumber =
+            String.padLeft 2 '0' (String.fromInt n)
     in
     div [ style "margin" "5px 0" ]
         [ a [ href ("/ex" ++ paddedNumber) ] [ text ("Exercise " ++ paddedNumber) ]
         ]
+
 
 mainPane : Model -> Html Msg
 mainPane model =
@@ -130,10 +195,21 @@ mainPane model =
                 case model.ex01Model of
                     Just ex01Model ->
                         Html.map Ex01Msg (Ex01.view ex01Model)
+
                     Nothing ->
                         text "Loading Ex01..."
+
+            Just 2 ->
+                case model.ex02Model of
+                    Just ex02Model ->
+                        Html.map Ex02Msg (Ex02.view ex02Model)
+
+                    Nothing ->
+                        text "Loading Ex02..."
+
             Just n ->
                 text ("Exercise " ++ String.fromInt n ++ " - Not implemented yet")
+
             Nothing ->
                 div []
                     [ h1 [] [ text "Welcome to Elm 57 Exercises" ]
@@ -141,12 +217,19 @@ mainPane model =
                     ]
         ]
 
+
+
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
-subscriptions _ = Sub.none
+subscriptions _ =
+    Sub.none
+
+
 
 -- MAIN
+
 
 main : Program () Model Msg
 main =
@@ -158,4 +241,3 @@ main =
         , onUrlRequest = LinkClicked
         , onUrlChange = UrlChanged
         }
-
---
+++ src/Pages/Ex02.elm
@@ -0,0 +1,87 @@
+module Pages.Ex02 exposing (Model, Msg(..), init, update, view)
+
+import Html exposing (Html, div, input, span, text)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (on, onInput)
+import Json.Decode as Decode
+import String exposing (fromInt, length)
+
+
+
+-- (fromInt, length)
+-- MODEL
+
+
+type alias Model =
+    { input : String
+    , message : String
+    }
+
+
+init : Model
+init =
+    { input = ""
+    , message = ""
+    }
+
+
+
+-- MSG
+
+
+type Msg
+    = InputChanged String
+    | Submit
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        InputChanged str ->
+            ( { model | input = str }, Cmd.none )
+
+        Submit ->
+            ( { model | message = makeOutput model.input }, Cmd.none )
+
+
+makeOutput : String -> String
+makeOutput input =
+    if input /= "" then
+        "Homer has " ++ fromInt (length input) ++ " characters."
+
+    else
+        ""
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div []
+            [ span [] [ text "What is the input string? " ]
+            , input
+                [ placeholder "Enter your name"
+                , value model.input
+                , onInput InputChanged
+                , on "keydown" (Decode.field "key" Decode.string |> Decode.andThen keyDecoder)
+                ]
+                []
+            ]
+        , div [] [ text model.message ]
+        ]
+
+
+keyDecoder : String -> Decode.Decoder Msg
+keyDecoder key =
+    if key == "Enter" then
+        Decode.succeed Submit
+
+    else
+        Decode.fail "Not Enter key"
