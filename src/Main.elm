054bd53cce784978d21c494b190afb4ff61f9eb9
yasuabe2613 <yasu@yasuabe.net>
2025-06-06 19:59:53 +0900

    add templates

--- src/Main.elm
+++ src/Main.elm
@@ -11,6 +11,59 @@ import Pages.Ex01 as Ex01
 import Pages.Ex02 as Ex02
 import Pages.Ex03 as Ex03
 import Pages.Ex04 as Ex04
+import Pages.Ex05 as Ex05
+import Pages.Ex06 as Ex06
+import Pages.Ex07 as Ex07
+import Pages.Ex08 as Ex08
+import Pages.Ex09 as Ex09
+import Pages.Ex10 as Ex10
+import Pages.Ex11 as Ex11
+import Pages.Ex12 as Ex12
+import Pages.Ex13 as Ex13
+import Pages.Ex14 as Ex14
+import Pages.Ex15 as Ex15
+import Pages.Ex16 as Ex16
+import Pages.Ex17 as Ex17
+import Pages.Ex18 as Ex18
+import Pages.Ex19 as Ex19
+import Pages.Ex20 as Ex20
+import Pages.Ex21 as Ex21
+import Pages.Ex22 as Ex22
+import Pages.Ex23 as Ex23
+import Pages.Ex24 as Ex24
+import Pages.Ex25 as Ex25
+import Pages.Ex26 as Ex26
+import Pages.Ex27 as Ex27
+import Pages.Ex28 as Ex28
+import Pages.Ex29 as Ex29
+import Pages.Ex30 as Ex30
+import Pages.Ex31 as Ex31
+import Pages.Ex32 as Ex32
+import Pages.Ex33 as Ex33
+import Pages.Ex34 as Ex34
+import Pages.Ex35 as Ex35
+import Pages.Ex36 as Ex36
+import Pages.Ex37 as Ex37
+import Pages.Ex38 as Ex38
+import Pages.Ex39 as Ex39
+import Pages.Ex40 as Ex40
+import Pages.Ex41 as Ex41
+import Pages.Ex42 as Ex42
+import Pages.Ex43 as Ex43
+import Pages.Ex44 as Ex44
+import Pages.Ex45 as Ex45
+import Pages.Ex46 as Ex46
+import Pages.Ex47 as Ex47
+import Pages.Ex48 as Ex48
+import Pages.Ex49 as Ex49
+import Pages.Ex50 as Ex50
+import Pages.Ex51 as Ex51
+import Pages.Ex52 as Ex52
+import Pages.Ex53 as Ex53
+import Pages.Ex54 as Ex54
+import Pages.Ex55 as Ex55
+import Pages.Ex56 as Ex56
+import Pages.Ex57 as Ex57
 import Url exposing (Url)
 import Url.Parser as Parser exposing ((</>), top)
 
@@ -26,6 +79,59 @@ type alias Model =
     , ex02Model : Maybe Ex02.Model
     , ex03Model : Maybe Ex03.Model
     , ex04Model : Maybe Ex04.Model
+    , ex05Model : Maybe Ex05.Model
+    , ex06Model : Maybe Ex06.Model
+    , ex07Model : Maybe Ex07.Model
+    , ex08Model : Maybe Ex08.Model
+    , ex09Model : Maybe Ex09.Model
+    , ex10Model : Maybe Ex10.Model
+    , ex11Model : Maybe Ex11.Model
+    , ex12Model : Maybe Ex12.Model
+    , ex13Model : Maybe Ex13.Model
+    , ex14Model : Maybe Ex14.Model
+    , ex15Model : Maybe Ex15.Model
+    , ex16Model : Maybe Ex16.Model
+    , ex17Model : Maybe Ex17.Model
+    , ex18Model : Maybe Ex18.Model
+    , ex19Model : Maybe Ex19.Model
+    , ex20Model : Maybe Ex20.Model
+    , ex21Model : Maybe Ex21.Model
+    , ex22Model : Maybe Ex22.Model
+    , ex23Model : Maybe Ex23.Model
+    , ex24Model : Maybe Ex24.Model
+    , ex25Model : Maybe Ex25.Model
+    , ex26Model : Maybe Ex26.Model
+    , ex27Model : Maybe Ex27.Model
+    , ex28Model : Maybe Ex28.Model
+    , ex29Model : Maybe Ex29.Model
+    , ex30Model : Maybe Ex30.Model
+    , ex31Model : Maybe Ex31.Model
+    , ex32Model : Maybe Ex32.Model
+    , ex33Model : Maybe Ex33.Model
+    , ex34Model : Maybe Ex34.Model
+    , ex35Model : Maybe Ex35.Model
+    , ex36Model : Maybe Ex36.Model
+    , ex37Model : Maybe Ex37.Model
+    , ex38Model : Maybe Ex38.Model
+    , ex39Model : Maybe Ex39.Model
+    , ex40Model : Maybe Ex40.Model
+    , ex41Model : Maybe Ex41.Model
+    , ex42Model : Maybe Ex42.Model
+    , ex43Model : Maybe Ex43.Model
+    , ex44Model : Maybe Ex44.Model
+    , ex45Model : Maybe Ex45.Model
+    , ex46Model : Maybe Ex46.Model
+    , ex47Model : Maybe Ex47.Model
+    , ex48Model : Maybe Ex48.Model
+    , ex49Model : Maybe Ex49.Model
+    , ex50Model : Maybe Ex50.Model
+    , ex51Model : Maybe Ex51.Model
+    , ex52Model : Maybe Ex52.Model
+    , ex53Model : Maybe Ex53.Model
+    , ex54Model : Maybe Ex54.Model
+    , ex55Model : Maybe Ex55.Model
+    , ex56Model : Maybe Ex56.Model
+    , ex57Model : Maybe Ex57.Model
     }
 
 
@@ -37,6 +143,59 @@ defaultModel key =
     , ex02Model = Nothing
     , ex03Model = Nothing
     , ex04Model = Nothing
+    , ex05Model = Nothing
+    , ex06Model = Nothing
+    , ex07Model = Nothing
+    , ex08Model = Nothing
+    , ex09Model = Nothing
+    , ex10Model = Nothing
+    , ex11Model = Nothing
+    , ex12Model = Nothing
+    , ex13Model = Nothing
+    , ex14Model = Nothing
+    , ex15Model = Nothing
+    , ex16Model = Nothing
+    , ex17Model = Nothing
+    , ex18Model = Nothing
+    , ex19Model = Nothing
+    , ex20Model = Nothing
+    , ex21Model = Nothing
+    , ex22Model = Nothing
+    , ex23Model = Nothing
+    , ex24Model = Nothing
+    , ex25Model = Nothing
+    , ex26Model = Nothing
+    , ex27Model = Nothing
+    , ex28Model = Nothing
+    , ex29Model = Nothing
+    , ex30Model = Nothing
+    , ex31Model = Nothing
+    , ex32Model = Nothing
+    , ex33Model = Nothing
+    , ex34Model = Nothing
+    , ex35Model = Nothing
+    , ex36Model = Nothing
+    , ex37Model = Nothing
+    , ex38Model = Nothing
+    , ex39Model = Nothing
+    , ex40Model = Nothing
+    , ex41Model = Nothing
+    , ex42Model = Nothing
+    , ex43Model = Nothing
+    , ex44Model = Nothing
+    , ex45Model = Nothing
+    , ex46Model = Nothing
+    , ex47Model = Nothing
+    , ex48Model = Nothing
+    , ex49Model = Nothing
+    , ex50Model = Nothing
+    , ex51Model = Nothing
+    , ex52Model = Nothing
+    , ex53Model = Nothing
+    , ex54Model = Nothing
+    , ex55Model = Nothing
+    , ex56Model = Nothing
+    , ex57Model = Nothing    
     }
 
 
---
+++ src/Pages/Ex14.elm
@@ -0,0 +1,49 @@
+module Pages.Ex14 exposing (Model, Msg(..), init, update, view)
+
+
+import Html exposing (Html, div, text)
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+init : Model
+init =
+    {}
+
+
+
+-- MSG
+
+
+type Msg
+    = Submit
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg _ =
+    case msg of
+        Submit ->
+            ( init
+            , Cmd.none
+            )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view _ =
+    div []
+        [ div [] [ text "not implemented" ]
+        ]
