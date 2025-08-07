edddb984796c7af0867fbd3edadd85e1d3838ce7
yasuabe2613 <yasu@yasuabe.net>
2025-08-07 16:00:30 +0900

    Ex46: ♻️ 💄refactor and UI brush-up

--- src/Pages/Ex46.elm
+++ src/Pages/Ex46.elm
@@ -3,13 +3,13 @@ module Pages.Ex46 exposing (Model, Msg(..), countWords, init, update, view)
 import Basics.Extra exposing (flip)
 import Common.CmdEx exposing (withNone)
 import Common.Function exposing (on)
-import Dict
+import Dict exposing (Dict)
 import File exposing (File)
 import File.Select as Select
-import Html exposing (Html, button, div, label, progress, text)
+import Html exposing (Html, button, div, span, text)
 import Html.Attributes as A exposing (class)
 import Html.Events exposing (onClick)
-import List.Extra as LX
+import List.Extra as LX exposing (count)
 import Maybe
 import Maybe.Extra as MX
 import Task
@@ -22,7 +22,7 @@ import Tuple exposing (second)
 
 defaultContents : String
 defaultContents =
-    "badger badger badger badger mushroom bear mushroom snake badger badger badger"
+    "badger badger badger badger mushroom mushroom snake badger badger badger"
 
 
 type alias Model =
@@ -36,10 +36,15 @@ init =
 
 countWords : Model -> List ( String, Int )
 countWords model =
+    let
+        countUp : String -> Dict String Int -> Dict String Int
+        countUp word =
+            Dict.update word (Maybe.map ((+) 1) >> flip MX.or (Just 1))
+    in
     model.contents
         |> String.split "\n"
         |> List.concatMap String.words
-        |> List.foldl (\word acc -> Dict.update word (Maybe.map ((+) 1) >> flip MX.or (Just 1)) acc) Dict.empty
+        |> List.foldl countUp Dict.empty
         |> Dict.toList
         |> List.sortWith (on (flip compare) second)
 
@@ -62,14 +67,10 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Submit ->
-            ( model
-            , Select.file [ "text/plain" ] FileSelected
-            )
+            ( model, Select.file [ "text/plain" ] FileSelected )
 
         FileSelected file ->
-            ( model
-            , Task.perform FileLoaded (File.toString file)
-            )
+            ( model, Task.perform FileLoaded (File.toString file) )
 
         FileLoaded contents_ ->
             withNone { model | contents = contents_ }
@@ -89,13 +90,9 @@ viewHistgram hist =
 
         line ( word, count ) =
             div []
-                [ label [] [ text word ]
-                , progress
-                    [ A.value (String.fromInt count)
-                    , A.max (String.fromInt maxCount)
-                    , A.style "width" "100px"
-                    ]
-                    []
+                [ span [] [ text word ]
+                , span [ A.style "width" (String.fromInt (count * 100 // maxCount) ++ "%") ] []
+                , span [ A.style "margin-left" "8px" ] [ text (String.fromInt count) ]
                 ]
     in
     div [ class "ex46__histogram" ] (List.map line hist)
@@ -104,11 +101,6 @@ viewHistgram hist =
 view : Model -> Html Msg
 view model =
     div [ class "ex46" ]
-        [ div []
-            [ button
-                [ onClick Submit ]
-                [ text "Upload" ]
-            ]
-        , div [ class "output" ]
-            [ viewHistgram (countWords model) ]
+        [ div [] [ button [ onClick Submit ] [ text "Upload" ] ]
+        , div [ class "output" ] [ viewHistgram (countWords model) ]
         ]
